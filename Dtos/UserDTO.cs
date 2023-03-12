0585084e3b60ce52157e26d328a92bab3e3d8dcf
Ish Gill <ish-gill@hotmail.com>
2023-03-13 10:58:01 +1300

    added prelim dbcontext class

--- Dtos/UserDTO.cs
+++ Dtos/UserDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace CodeSaint.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+using CodeSaint.Models;
+
+namespace CodeSaint.Models
 {
     public class UserDTO
     {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Country { get; set; }
+        public string? Gender { get; set; }
+        public string? Age { get; set; }
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace CodeSaint.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeSaint.Models
 {
     public class User
     {
+        [Key]
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Country { get; set; }
+        public string? Gender { get; set; }
+        public string? Age { get; set; }
     }
 }
